1c66f4d31f8abde3daaacb7c88d457be8bbf99df
nipsalvin <amwaniki.am@gmail.com>
2022-01-09 12:57:56 +0300

    Chapter 3

---
+++ Chapter 3 Types and Values/mytypes.py
@@ -0,0 +1,15 @@
+a = 7
+b = 7.1
+c = "Alvin"
+
+print("a is {}".format(a))
+#print(f"a is {a}")
+print(type(a))
+
+print("b is {}".format(b))
+#print(f"b is {b})
+print(type(b))
+
+print("c is {}".format(c))
+print(f"c is {c}")
+print(f"c is {c} which is a", type(c))
