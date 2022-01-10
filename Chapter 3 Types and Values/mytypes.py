c954fc0651fbb071f793124427fd304c990abbbd
nipsalvin <amwaniki.am@gmail.com>
2022-01-10 11:12:08 +0300

    Chapter 3

--- Chapter 3 Types and Values/mytypes.py
+++ Chapter 3 Types and Values/mytypes.py
@@ -1,6 +1,20 @@
 a = 7
 b = 7.1
 c = "Alvin"
+d = "nipsAlvin".capitalize()
+"""
+.capitalize - capitalizes first letter
+.upper - makes it all upper case
+.lower - makes it all lower case
+"""
+e= "seven {} {}".format(8, 9)
+#place holders are filled with positional arguments 8 and 9 respectively
+f = "seven {1} {0}".format(8, 9)
+#positions are swapped since they are declared in the arguments
+g = "seven {0:<9} {1:>9}".format(8, 9)
+# :<9 and :>9 add 9 spaces to the left and right respectively
+h = "seven {0:<09} {1:>09}".format(8, 123456789123456789)
+#this adds 9 zeros to the spaces
 
 print("a is {}".format(a))
 #print(f"a is {a}")
@@ -13,3 +27,8 @@ print(type(b))
 print("c is {}".format(c))
 print(f"c is {c}")
 print(f"c is {c} which is a", type(c))
+print(f"This is {d}")
+print (e)
+print (f)
+print (g)
+print (h)
