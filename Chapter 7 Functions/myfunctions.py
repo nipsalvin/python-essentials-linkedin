c4945fe4f07d2a03bbf98278b8e1abee09f49b51
Nipsalvin <amwaniki.am@gmail.com>
2022-01-18 21:47:01 +0300

    functions

---
+++ Chapter 7 Functions/myfunctions.py
@@ -0,0 +1,39 @@
+def main ():
+    kitten()
+
+def kitten():
+    print("meow")   
+
+
+"""
+def main ():
+    kitten(3)
+
+def kitten(n):
+    print(f"{n} meow")   
+
+#this prints out (3 Meow)
+"""
+
+"""
+def main ():
+    x = kitten()
+    print(x)
+
+def kitten():
+    print("meow")
+#This returns meow and none because no value is being returned
+"""
+
+"""
+def main ():
+    x = kitten()
+    print(x)
+
+def kitten():
+    return"meow"
+#This returns meow
+"""
+
+
+if __name__ == "__main__":main()
